f7657836a90219020a89f3d6dfd8f23eda80f18b
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-12 21:44:11 +0530

    Add files

--- Makefile
+++ Makefile
@@ -1,5 +1,18 @@
-all:
+FILES = ./build/kernel.asm.o
+
+all: ./bin/boot.bin ./bin/kernel.bin
+	rm -rf ./bin/os.bin
+	dd if=./bin/boot.bin >> ./bin/os.bin
+
+./bin/kernel.bin: $(FILES)
+	i686-elf-ld -g -relocatable $(FILES) -o ./build/kernelfull.o
+	i686-elf-gcc -T ./src/linker.ld -o ./bin/kernel.bin -ffreestanding -O0 -nostdlib ./build/kernelfull.o
+
+./bin/boot.bin: ./src/boot/boot.asm
 	nasm -f bin ./src/boot/boot.asm -o ./bin/boot.bin
+
+./build/kernel.asm.o: ./src/kernel.asm
+	nasm -f elf -g ./src/kernel.asm -o ./build/kernel.asm.o
 
 clean:
 	rm -rf ./bin/boot.bin
--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -28,8 +28,9 @@ step2:
     mov eax, cr0
     or eax, 0x1
     mov cr0, eax
-    jmp CODE_SEG:load32
-
+    ;jmp CODE_SEG:load32
+    jmp $
+    
 ; GDT
 gdt_start:
 gdt_null:
@@ -60,23 +61,6 @@ gdt_descriptor:
     dw gdt_end - gdt_start - 1
     dd gdt_start
 
-[BITS 32]
-load32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov ebp, 0x00200000
-    mov esp, ebp
-
-    ; Enable the A20 line
-    in al, 0x92
-    or al, 2
-    out 0x92, al
-
-    jmp $
 
 
 times 510-($-$$) db 0   ; Fill the rest of the sector with zeros
