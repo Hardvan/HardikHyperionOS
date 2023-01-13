7732a92c6973c40d87e774923db72313ccdb8a12
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-13 17:11:29 +0530

    Complete Writing to the Screen

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,8 @@
 #include <stddef.h>
 
 uint16_t *video_memory = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
 
 // Takes a character and a color and returns a 16-bit value
 uint16_t terminal_make_char(char c, char color)
@@ -15,9 +17,29 @@ void terminal_putchar(int x, int y, char c, char color)
     video_memory[y * VGA_WIDTH + x] = terminal_make_char(c, color);
 }
 
+void terminal_writechar(char c, char color)
+{
+    if (c == '\n')
+    {
+        terminal_col = 0;
+        terminal_row++;
+        return;
+    }
+
+    terminal_putchar(terminal_col, terminal_row, c, color);
+    terminal_col++;
+    if (terminal_col >= VGA_WIDTH)
+    {
+        terminal_col = 0;
+        terminal_row++;
+    }
+}
+
 void terminal_initialize()
 {
     video_memory = (uint16_t *)(0xB8000);
+    terminal_row = 0;
+    terminal_col = 0;
     for (int y = 0; y < VGA_HEIGHT; y++)
     {
         for (int x = 0; x < VGA_WIDTH; x++)
@@ -32,13 +54,25 @@ size_t strlen(const char *str)
     size_t len = 0;
 
     while (str[len])
+    {
         len++;
+    }
 
     return len;
+}
+
+void print(const char *str)
+{
+    size_t len = strlen(str);
+    for (size_t i = 0; i < len; i++)
+    {
+        terminal_writechar(str[i], 15);
+    }
 }
 
 void kernel_main()
 {
     terminal_initialize();
-    video_memory[0] = terminal_make_char('A', 15);
+
+    print("Hello, World!\nThis is written in C!");
 }
