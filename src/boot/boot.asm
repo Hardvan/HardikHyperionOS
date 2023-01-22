5ff2189c6486fb1e2d202890c1acf1ecd0fb5a01
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-22 18:20:54 +0530

    Start FAT File System

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -4,11 +4,34 @@ BITS 16
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
-_start:
-    jmp short start
-    nop
+jmp short start
+nop
 
- times 33 db 0 ; 33 bytes of zeros
+; FAT16 Header
+OEMIdentifier       db 'HARDIOS ' ; 8 bytes (named HARDI on purpose)
+BytesPerSector      dw 0x200
+SectorsPerCluster   db 0x80
+ReservedSectors     dw 200
+FATCopies           db 0x02
+RootDirEntries      dw 0x40
+NumSectors          dw 0x00
+MediaType           db 0xF8
+SectorsPerFat       dw 0x100
+SectorsPerTrack     dw 0x20
+NumberOfHeads       dw 0x40
+HiddenSectors       dd 0x00
+SectorsBig          dd 0x773594
+
+; Extended BPB (Dos 4.0)
+DriveNumber         db 0x80
+WinNTBit            db 0x00
+Signature           db 0x29
+VolumeID            dd 0xD105
+VolumeIDString      db 'HARDIOS BOO' ; 11 bytes (named HARDIOS BOO on purpose)
+SystemIDString      db 'FAT16   ' ; 8 bytes (named FAT16 on purpose)
+
+
+
 
 start:
     jmp 0:step2
