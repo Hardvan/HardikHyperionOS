8a5a105cf4854ea939f90963a824cd71526bdf69
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-22 11:04:41 +0530

    Create a disk stream

--- src/kernel.c
+++ src/kernel.c
@@ -8,6 +8,7 @@
 #include "disk/disk.h"
 #include "fs/pparser.h"
 #include "string/string.h"
+#include "disk/streamer.h"
 
 uint16_t *video_memory = 0;
 uint16_t terminal_row = 0;
@@ -99,8 +100,11 @@ void kernel_main()
     // Enable interrupts
     enable_interrupts();
 
-    struct path_root *root_path = pathparser_parse("0:/bin/shell.exe", NULL);
-    if (root_path)
+    struct disk_stream *stream = diskstreamer_new(0);
+    diskstreamer_seek(stream, 0x201);
+    unsigned char c = 0;
+    diskstreamer_read(stream, &c, 1);
+    while (1)
     {
     }
 }
