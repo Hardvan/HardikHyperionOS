1c614786c4c527809ad5d2212d75c5168d030141
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-23 17:34:50 +0530

    Implement Task Foundations

---
+++ src/task/task.c
@@ -0,0 +1,115 @@
+#include "task.h"
+#include "kernel.h"
+#include "status.h"
+#include "config.h"
+#include "memory/heap/kheap.h"
+#include "memory/memory.h"
+
+struct task *current_task = 0;
+
+// Task Linked List
+struct task *task_tail = 0;
+struct task *task_head = 0;
+
+struct task *task_current()
+{
+    return current_task;
+}
+
+int task_init(struct task *task);
+
+struct task *task_new()
+{
+    int res = 0;
+    struct task *task = kzalloc(sizeof(struct task));
+    if (!task)
+    {
+        res = -ENOMEM;
+        goto out;
+    }
+
+    res = task_init(task);
+    if (res != HARDIKHYPERIONOS_ALL_OK)
+    {
+        goto out;
+    }
+
+    if (task_head == 0)
+    {
+        task_head = task;
+        task_tail = task;
+        goto out;
+    }
+
+    task_tail->next = task;
+    task->prev = task_tail;
+    task_tail = task;
+
+out:
+    if (IS_ERR(res))
+    {
+        task_free(task);
+        return ERROR(res);
+    }
+
+    return task;
+}
+
+struct task *task_get_next()
+{
+    if (!current_task->next)
+    {
+        return task_head;
+    }
+
+    return current_task->next;
+}
+
+static void task_list_remove(struct task *task)
+{
+    if (task->prev)
+    {
+        task->prev->next = task->next;
+    }
+
+    if (task == task_head)
+    {
+        task_head = task->next;
+    }
+
+    if (task == task_tail)
+    {
+        task_tail = task->prev;
+    }
+
+    if (task == current_task)
+    {
+        current_task = task_get_next();
+    }
+}
+
+int task_free(struct task *task)
+{
+    paging_free_4gb(task->page_directory);
+    task_list_remove(task);
+
+    kfree(task);
+
+    return 0;
+}
+
+int task_init(struct task *task)
+{
+    memset(task, 0, sizeof(struct task));
+    task->page_directory = paging_new_4gb(PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
+    if (!task->page_directory)
+    {
+        return -EIO;
+    }
+
+    task->registers.ip = HARDIKHYPERION_OS_VIRTUAL_ADDRESS;
+    task->registers.ss = USER_DATA_SEGMENT;
+    task->registers.esp = HARDIKHYPERIONOS_PROGRAM_VIRTUAL_STACK_ADDRESS_START;
+
+    return 0;
+}
