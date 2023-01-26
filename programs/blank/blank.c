a6c8a74097278615bef9e747d8d0be02156d1602
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-26 16:21:56 +0530

    Create a shell

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -6,20 +6,6 @@ int main(int argc, char **argv)
 {
     printf("My age is %i\n", 19);
 
-    print("Hello how are you!\n");
-
-    print(itoa(8763));
-
-    putchar('Z');
-
-    void *ptr = malloc(512);
-    free(ptr);
-
-    char buf[1024];
-    hardikhyperionos_terminal_readline(buf, sizeof(buf), true);
-
-    print(buf);
-
     while (1)
     {
     }
---
+++ programs/shell/src/shell.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "hardikhyperionos.h"
+
+int main(int argc, char **argv)
+{
+    print("HardikHyperionOS v1.0.0\n");
+
+    while (1)
+    {
+        print("> ");
+        char buf[1024];
+        hardikhyperionos_terminal_readline(buf, sizeof(buf), true);
+        print("\n");
+    }
+
+    return 0;
+}
