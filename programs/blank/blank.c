cdaf9fe311297f7f2a8ba37754076134e0a44a23
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-26 15:41:57 +0530

    Implement itoa function

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -5,6 +5,8 @@ int main(int argc, char **argv)
 {
     print("Hello how are you!\n");
 
+    print(itoa(8763));
+
     void *ptr = malloc(512);
     free(ptr);
 
--- programs/stdlib/src/stdlib.h
+++ programs/stdlib/src/stdlib.h
@@ -5,5 +5,6 @@
 
 void *malloc(size_t size);
 void free(void *ptr);
+char *itoa(int i);
 
 #endif
