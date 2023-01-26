ce31aa481f4a9db1177abf7c586f0267777571b8
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-26 13:24:42 +0530

    Create virtual keyboard layer

--- src/task/process.h
+++ src/task/process.h
@@ -26,9 +26,18 @@ struct process
 
     // Size of the data pointed to by ptr
     uint32_t size;
+
+    struct keyboard_buffer
+    {
+        char buffer[HARDIKHYPERIONOS_KEYBOARD_BUFFER_SIZE];
+        int tail;
+        int head;
+    } keyboard;
 };
 
 int process_load(const char *filename, struct process **process);
 int process_load_for_slot(const char *filename, struct process **process, int process_slot);
+struct process *process_current();
+struct process *process_get(int process_id);
 
 #endif
