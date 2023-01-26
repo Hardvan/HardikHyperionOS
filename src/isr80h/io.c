83fa042b58b4f135021952ab3f2517dbe05d64ec
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-26 14:01:36 +0530

    Create a putchar command

--- src/isr80h/io.c
+++ src/isr80h/io.c
@@ -20,3 +20,11 @@ void *isr80h_command2_getkey(struct interrupt_frame *frame)
 
     return (void *)((int)c);
 }
+
+void *isr80h_command3_putchar(struct interrupt_frame *frame)
+{
+    char c = (char)(int)task_get_stack_item(task_current(), 0);
+    terminal_writechar(c, 15);
+
+    return 0;
+}
