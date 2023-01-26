374a1a011bf9e7bdbc0daa7c30664a0e0b2934b5
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-26 15:11:02 +0530

    Implement system print in stdlib

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,4 +1,11 @@
+#include "hardikhyperionos.h"
+
 int main(int argc, char **argv)
 {
+    print("Hello how are you!\n");
+
+    while (1)
+    {
+    }
     return 0;
 }
---
+++ programs/stdlib/src/hardikhyperionos.asm
@@ -0,0 +1,14 @@
+[BITS 32]
+
+global print:function
+
+; void print(const char* filename)
+print:
+    push ebp
+    mov ebp, esp
+    push dword[ebp+8]
+    mov eax, 1 ; Command print
+    int 0x80
+    add esp, 4
+    pop ebp
+    ret
