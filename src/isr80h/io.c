4ee0399a986bfdeac7f43de67e5e03d51c4d5cb9
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-26 13:57:47 +0530

    Get a key from keyboard buffer in user land

--- src/isr80h/io.c
+++ src/isr80h/io.c
@@ -1,6 +1,7 @@
 #include "io.h"
 #include "task/task.h"
 #include "kernel.h"
+#include "keyboard/keyboard.h"
 
 void *isr80h_command1_print(struct interrupt_frame *frame)
 {
@@ -11,4 +12,11 @@ void *isr80h_command1_print(struct interrupt_frame *frame)
     print(buf);
 
     return 0;
+}
+
+void *isr80h_command2_getkey(struct interrupt_frame *frame)
+{
+    char c = keyboard_pop();
+
+    return (void *)((int)c);
 }
