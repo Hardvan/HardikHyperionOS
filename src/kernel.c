adec3aed3ab26b93bd9df0dd817c144d63c523c2
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-26 13:53:27 +0530

    Complete PS2 Port Keyboard driver part 2

--- src/kernel.c
+++ src/kernel.c
@@ -107,11 +107,6 @@ struct gdt_structured gdt_structured[HARDIKHYPERIONOS_TOTAL_GDT_SEGMENTS] = {
     {.base = (uint32_t)&tss, .limit = sizeof(tss), .type = 0xE9} // TSS segment
 };
 
-void pic_timer_callback(struct interrupt_frame *frame)
-{
-    print("Timer activated\n");
-}
-
 // The kernel main function
 void kernel_main()
 {
@@ -162,16 +157,12 @@ void kernel_main()
     // Initialize all the system keyboards
     keyboard_init();
 
-    idt_register_interrupt_callback(0x20, pic_timer_callback);
-
     struct process *process = 0;
     int res = process_load_switch("0:/blank.bin", &process);
     if (res != HARDIKHYPERIONOS_ALL_OK)
     {
         panic("Failed to load blank.bin\n");
     }
-
-    keyboard_push('A');
 
     task_run_first_ever_task();
 
--- src/keyboard/classic.c
+++ src/keyboard/classic.c
@@ -1,6 +1,10 @@
 #include "classic.h"
 #include "keyboard.h"
 #include "io/io.h"
+#include "kernel.h"
+#include "idt/idt.h"
+#include "task/task.h"
+
 #include <stdint.h>
 #include <stddef.h>
 
@@ -24,8 +28,12 @@ struct keyboard classic_keyboard = {
     .name = {"Classic"},
     .init = classic_keyboard_init};
 
+void classic_keyboard_handle_interrupt();
+
 int classic_keyboard_init()
 {
+    idt_register_interrupt_callback(ISR_KEYBOARD_INTERRUPT, classic_keyboard_handle_interrupt);
+
     outb(PS2_PORT, PS2_COMMAND_ENABLE_FIRST_PORT);
 
     return 0;
@@ -44,8 +52,25 @@ uint8_t classic_keyboard_scancode_to_char(uint8_t scancode)
     return c;
 }
 
-void clasic_keyboard_handle_interrupt()
+void classic_keyboard_handle_interrupt()
 {
+    kernel_page();
+    uint8_t scancode = 0;
+    scancode = insb(KEYBOARD_INPUT_PORT);
+    insb(KEYBOARD_INPUT_PORT);
+
+    if (scancode & CLASSIC_KEYBOARD_KEY_RELEASED)
+    {
+        return;
+    }
+
+    uint8_t c = classic_keyboard_scancode_to_char(scancode);
+    if (c != 0)
+    {
+        keyboard_push(c);
+    }
+
+    task_page();
 }
 
 struct keyboard *classic_init()
