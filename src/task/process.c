5b45f154b97da4b6a3c4df6193a0420a15734921
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-01-26 15:38:10 +0530

    Change way of mapping of virtual pages

--- src/task/process.c
+++ src/task/process.c
@@ -221,7 +221,7 @@ static int process_map_elf(struct process *process)
         res = paging_map_to(process->task->page_directory,
                             paging_align_to_lower_page((void *)phdr->p_vaddr),
                             paging_align_to_lower_page(phdr_phys_address),
-                            paging_align_address(phdr_phys_address + phdr->p_filesz),
+                            paging_align_address(phdr_phys_address + phdr->p_memsz),
                             flags);
         if (IS_ERR(res))
         {
