6a0e9a491854f8103b40a103db67c7ad3a7e5a2e
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-12 22:58:01 +0530

    Make blank.c run infinite times

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -7,8 +7,8 @@
 
 int main(int argc, char **argv)
 {
-    // Display the argument 50 times
-    for (int a = 0; a < 50; a++)
+    // Display the argument infinite times
+    for (int a = 0;; a++)
     {
         print(argv[0]);
         for (int i = 0; i < 100000; i++) // Delay
